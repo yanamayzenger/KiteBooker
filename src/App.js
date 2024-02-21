6b5c87483f462e1c831eda1576d9afd946e6bb6b
Yana Mayzenger <55948608+yanamayzenger@users.noreply.github.com>
2024-02-20 20:06:03 -0500

    calendar component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from "./logo.svg";
-import "./App.scss";
+import React, { useState } from "react";
+import Calendar from "./components/Calendar/Calendar";
 
 function App() {
+  const [date, setDate] = useState(new Date());
+
+  const handleSelectDate = (selectedDate) => {
+    setDate(selectedDate);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>KiteBooker: Book Your Kitesurfing Lesson</h1>
+      <Calendar onSelectDate={handleSelectDate} />
     </div>
   );
 }
