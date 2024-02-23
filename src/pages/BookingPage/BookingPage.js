79b45ccdbbce5d283ba1fd3f13012ae07a39d600
Yana Mayzenger <55948608+yanamayzenger@users.noreply.github.com>
2024-02-22 22:27:34 -0500

    Footer Completed

--- src/pages/BookingPage/BookingPage.js
+++ src/pages/BookingPage/BookingPage.js
@@ -1,26 +1,30 @@
 import React from "react";
 import Header from "../../components/Header/Header";
+import Footer from "../../components/Footer/Footer";
 import CustomCalendar from "../../components/Calendar/Calendar";
 import LessonTimeAvailability from "../../components/TimeSlots/TimeSlots";
 import WeatherWidget from "../../components/Weather/Weather";
 import InstructorsProfile from "../../components/InstructorList/InstructorList";
-
 import "./BookingPage.scss";
 
 const BookingPage = () => (
-  <div className="booking">
-    <div className="left-column">
-      <div className="calendar-and-slots">
-        <CustomCalendar />
-        <LessonTimeAvailability />
+  <div className="booking-page">
+    <Header />
+    <div className="booking-content">
+      <div className="left-column">
+        <div className="calendar-and-slots">
+          <CustomCalendar />
+          <LessonTimeAvailability />
+        </div>
+        <div className="left-bottom">
+          <WeatherWidget />
+        </div>
       </div>
-      <div className="left-bottom">
-        <WeatherWidget />
+      <div className="right-column">
+        <InstructorsProfile />
       </div>
     </div>
-    <div className="right-column">
-      <InstructorsProfile />
-    </div>
+    <Footer />
   </div>
 );
 
