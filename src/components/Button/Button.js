851026c10bfdab0c9af5d74abcbf5ad5bc9d52d7
Yana Mayzenger <55948608+yanamayzenger@users.noreply.github.com>
2024-02-24 10:19:25 -0500

    Revert "Added Timeslots "

--- src/components/Button/Button.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Button.scss";
-
-const ButtonComponent = ({ title }) => {
-  return <button className="custom-button">{title}</button>;
-};
-
-export default ButtonComponent;
