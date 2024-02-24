199259fb672557e2c7af6f16fc1ded95ff158e5c
Yana Mayzenger <55948608+yanamayzenger@users.noreply.github.com>
2024-02-24 16:49:43 -0500

    added InstructorsPage

---
+++ src/components/InstructorsFullList/InstructorsFullList.js
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import InstructorCardFull from "../InstructorCardFull/InstructorCardFull";
+import "./InstructorsFullList.scss";
+import { instructorImages } from "../InstructorImages/InstructorImages";
+
+function InstructorFullList() {
+  const [instructors, setInstructors] = useState([]);
+
+  useEffect(() => {
+    fetch(`http://localhost:8080/api/instructors`)
+      .then((response) => response.json())
+      .then((data) => {
+        setInstructors(
+          data.map((instructor) => {
+            return {
+              ...instructor,
+              image: instructorImages[instructor.name],
+            };
+          })
+        );
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
+  return (
+    <div className="instructors-list-container">
+      <div className="instructors-list-profiles">
+        {instructors.map((instructor) => (
+          <InstructorCardFull key={instructor.id} {...instructor} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default InstructorFullList;
